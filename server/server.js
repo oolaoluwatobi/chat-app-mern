e85ba0a6a24e51f0706123fbb4ab59991995ecd6
oolaoluwatobi <58662625+oolaoluwatobi@users.noreply.github.com>
2024-03-20 10:28:51 +0100

    Update file path for serving index.html

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
